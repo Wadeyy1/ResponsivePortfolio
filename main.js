ad542c5e0bcd6fece153dda801654299cd3ce0bb
Jack Wade <jackwade19@gmail.com>
2021-05-08 21:54:17 +0100

    Updated portfolio items and tidied up some of the html. Aded a fav icon to the application aswell.

--- main.js
+++ main.js
@@ -32,3 +32,24 @@ $(document).ready(function() {
       $('.modal').toggleClass('modal--hidden');
     });
 });
+
+var modal = document.getElementById("BugDemoModal");
+var btn = document.getElementById("ViewBugDemo");
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
